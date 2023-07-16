14b15b3e69fce5848c3d6cd6d23eaaacde01c791
Brandon Ye <bye4242@gmai.com>
2023-07-16 18:14:55 -0500

    feature: added menu to select specific sort type

--- main.py
+++ main.py
@@ -9,26 +9,15 @@ amount_nums = 50
 arr = np.random.randint(0, 50, amount_nums)
 arr_sorted = sorted(arr)
 
-# Create subplots
-fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
-bubble_sort_bars = ax1.bar(range(len(arr)), arr)
-ax1.title.set_text("Bubble Sort")
-
-insertion_sort_bars = ax2.bar(range(len(arr)), arr)
-ax2.title.set_text("Insertion Sort")
-
-merge_sort_bars = ax3.bar(range(len(arr)), arr)
-ax3.title.set_text("Merge Sort")
-
 # Bubble sort algorithm
 def bubbleSort(arr_copy):
     n = len(arr_copy)
     for i in range(0, n):
+        yield arr_copy.copy()
         for j in range(0, n - i - 1):
             if arr_copy[j] > arr_copy[j + 1]:
                 arr_copy[j], arr_copy[j + 1] = arr_copy[j + 1], arr_copy[j]
             yield arr_copy.copy()
-        yield arr_copy.copy()
     yield arr_copy.copy()
 
 # Insertion sort algorithim
@@ -46,6 +35,7 @@ def insertionSort(arr_copy):
 
         arr_copy[j + 1] = key
         yield arr_copy.copy()
+    yield arr_copy.copy()
 
 # function to recursively divide the arra
 def mergesort(arr_copy, start, end):
@@ -96,35 +86,86 @@ def update_fig(frame, bars):
         for rect, val in zip(bars, frame):
             rect.set_color('green')
     return bars
+global anim
+# Create subplots
+#sorting_type = input("Bubble Sort = 1, Insertion Sort = 2, Merge Sort = 3: ")
+fig, ax = plt.subplots()
+sorting_type = None
+# if sorting_type == "1":
+#     bubble_sort_bars = ax.bar(range(len(arr)), arr)
+#     ax.title.set_text("Bubble Sort")
+#     anim = FuncAnimation(fig, update_fig, frames=bubbleSort(arr.copy()), fargs=(bubble_sort_bars,), interval=20, repeat=False, blit=True, save_count=amount_nums)
+# elif sorting_type == "2":
+#     insertion_sort_bars = ax.bar(range(len(arr)), arr)
+#     ax.title.set_text("Insertion Sort")
+#     anim = FuncAnimation(fig, update_fig, frames=insertionSort(arr.copy()), fargs=(insertion_sort_bars,), interval=20, repeat=False, blit=True, save_count=amount_nums)
+# elif sorting_type == "3":
+#     merge_sort_bars = ax.bar(range(len(arr)), arr)
+#     ax.title.set_text("Merge Sort")
+#     anim3 = FuncAnimation(fig, update_fig, frames=mergesort(arr.copy(), 0, amount_nums - 1), fargs=(merge_sort_bars,), interval=20, repeat=False, blit=True, save_count=amount_nums)
 
-anim1 = FuncAnimation(fig, update_fig, frames=bubbleSort(arr.copy()), fargs=(bubble_sort_bars,), interval=20, repeat=False, blit=True, save_count=amount_nums)
-anim2 = FuncAnimation(fig, update_fig, frames=insertionSort(arr.copy()), fargs=(insertion_sort_bars,), interval=20, repeat=False, blit=True, save_count=amount_nums)
-anim3 = FuncAnimation(fig, update_fig, frames=mergesort(arr.copy(), 0, amount_nums - 1), fargs=(merge_sort_bars,), interval=20, repeat=False, blit=True, save_count=amount_nums)
+    
 
-def pause(event):
+bubble_sort_bars = ax.bar(range(len(arr)), arr)
+ax.title.set_text("Unsorted Array")
+
+def bubble_sort_show(event):
+    global anim
     try:
-        anim1.event_source.stop()
-        anim2.event_source.stop()
-        anim3.event_source.stop()
+        anim.event_source.stop()
     except:
-        print("One or more animations is done running")
-        
+        ax.clear()
+    finally:
+        bubble_sort_bars = ax.bar(range(len(arr)), arr)
+        ax.title.set_text("Bubble Sort")
+        anim = FuncAnimation(fig, update_fig, frames=bubbleSort(arr.copy()), fargs=(bubble_sort_bars,), interval=20, repeat=False, blit=True, save_count=amount_nums)
 
-def start(event):
+def insertion_sort_show(event):
+    global anim
     try:
-        anim1.event_source.start()
-        anim2.event_source.start()
-        anim3.event_source.start()
+        anim.event_source.stop()
     except:
-        print("One or more animations is done running")
- 
+        ax.clear()
+    finally:
+        insertion_sort_bars = ax.bar(range(len(arr)), arr, color = "black")
+        ax.title.set_text("Insertion Sort")
+        anim = FuncAnimation(fig, update_fig, frames=insertionSort(arr.copy()), fargs=(insertion_sort_bars,), interval=20, repeat=False, blit=True, save_count=amount_nums)
+        plt.show()
 
-axpause = fig.add_axes([0.87, 0.0, 0.1, 0.05])
-button_pause = Button(axpause, 'Pause')
-button_pause.on_clicked(pause)
+def merge_sort_show(event):
+    global anim
+    try:
+        anim.event_source.stop()
+    except:
+        ax.clear()
+    finally:
+        merge_sort_bars = ax.bar(range(len(arr)), arr)
+        ax.title.set_text("Merge Sort")
+        anim = FuncAnimation(fig, update_fig, frames=mergesort(arr.copy(), 0, amount_nums - 1), fargs=(merge_sort_bars,), interval=20, repeat=False, blit=True, save_count=amount_nums)
+        plt.show()
 
-axstart = fig.add_axes([0.75, 0.0, 0.1, 0.05])
-button_start = Button(axstart, 'Start')
-button_start.on_clicked(start)
+def merge_sort_show(event):
+    global anim
+    try:
+        anim.event_source.stop()
+    except:
+        ax.clear()
+    finally:
+        merge_sort_bars = ax.bar(range(len(arr)), arr)
+        ax.title.set_text("Merge Sort")
+        anim = FuncAnimation(fig, update_fig, frames=mergesort(arr.copy(), 0, amount_nums - 1), fargs=(merge_sort_bars,), interval=20, repeat=False, blit=True, save_count=amount_nums)
+        plt.show()
+
+button_bubble = plt.axes([0.6, 0.02, 0.1, 0.05])
+button_bubble_sort = Button(button_bubble, 'Bubble Sort')
+button_bubble_sort.on_clicked(bubble_sort_show)
+
+button_merge = plt.axes([0.4, 0.02, 0.1, 0.05])
+button_merge_sort = Button(button_merge, 'Merge Sort')
+button_merge_sort.on_clicked(merge_sort_show)
+
+button_insertion = plt.axes([0.2, 0.02, 0.1, 0.05])
+button_insertion_sort = Button(button_insertion, 'Insertion Sort')
+button_insertion_sort.on_clicked(insertion_sort_show)
 
 plt.show()
