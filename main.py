235f8bbb59da14259af42b16270ddc62413b2cad
Brandon Ye <bye4242@gmai.com>
2023-07-15 20:55:49 -0500

    feature: visualization for bubble sort

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
+amount_nums = 25
+
+arr = np.random.randint(0, 100, amount_nums)
+x = np.arange(0, amount_nums, 1)
+n = len(arr)
+
+#Bubble sort, if element is larger move it to the right
+for i in range(0, n):
+    for j in range(0, n-i-1):
+        plt.bar(x, arr)
+        plt.pause(0.01)
+        plt.clf()
+        if arr[j] > arr[j + 1]:
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+plt.show()
