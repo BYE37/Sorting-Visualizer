aa0a4282e8172802fbba0592fd879b045f18642d
Brandon Ye <bye4242@gmai.com>
2023-07-17 20:26:25 -0500

    refactor: reduced amount of functions and implemented more reusability for clearer code

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ amount_nums = 50
 # Generate random data, and create global variables shared between all functions
 global arr
 global anim
-anim = None
 arr = np.random.randint(1, 30, amount_nums)
 
 # Bubble sort algorithm
@@ -39,7 +38,7 @@ def insertionSort(arr_copy):
     yield arr_copy.copy()
 
 # function to recursively divide the arra
-def mergesort(arr_copy, start, end):
+def mergeSort(arr_copy, start = 0, end = amount_nums - 1):
     if end <= start:
         return
   
@@ -47,8 +46,8 @@ def mergesort(arr_copy, start, end):
       
     # yield from statements have been used to yield 
     # the array from the functions 
-    yield from mergesort(arr_copy, start, mid)
-    yield from mergesort(arr_copy, mid + 1, end)
+    yield from mergeSort(arr_copy, start, mid)
+    yield from mergeSort(arr_copy, mid + 1, end)
     yield from merge(arr_copy, start, mid, end)
   
 # function to merge the array
@@ -85,80 +84,75 @@ def update_fig(frame, bars):
         rect.set_height(val)
 
     if all(frame[i] == arr_sorted[i] for i in range(len(frame) - 1)):
-        for rect, val in zip(bars, frame):
+        for rect in bars:
             rect.set_color('lime')
+
     return bars
 
 fig, ax = plt.subplots()
-ax.bar(range(len(arr)), arr, color = "black")
-ax.title.set_text("Unsorted Array")
 
-def bubble_sort_show(event):
+#show type of sort
+def sort_show(sort_type):
     global anim
     if anim is not None and anim.event_source is not None:
         anim.event_source.stop()
+    
     ax.clear()
-    bubble_sort_bars = ax.bar(range(len(arr)), arr, color = "black")
-    ax.title.set_text("Bubble Sort")
-    anim = FuncAnimation(fig, update_fig, frames=bubbleSort(arr.copy()), fargs=(bubble_sort_bars,), interval=20, repeat=False, blit=True, save_count=amount_nums)
-
-def insertion_sort_show(event):
-    global anim
-    if anim is not None and anim.event_source is not None:
-        anim.event_source.stop()
-
-    ax.clear()
-    insertion_sort_bars = ax.bar(range(len(arr)), arr, color = "black")
-    ax.title.set_text("Insertion Sort")
-    anim = FuncAnimation(fig, update_fig, frames=insertionSort(arr.copy()), fargs=(insertion_sort_bars,), interval=20, repeat=False, blit=True, save_count=amount_nums)
-    plt.show()
-
-def merge_sort_show(event):
-    global anim
-    if anim is not None and anim.event_source is not None:
-        anim.event_source.stop()
-
-    ax.clear()
-    merge_sort_bars = ax.bar(range(len(arr)), arr, color = "black")
-    ax.title.set_text("Merge Sort")
-    anim = FuncAnimation(fig, update_fig, frames=mergesort(arr.copy(), 0, amount_nums - 1), fargs=(merge_sort_bars,), interval=20, repeat=False, blit=True, save_count=amount_nums)
-    plt.show()
-
-def unsorted_array_show(event):
-    global anim
-    if anim is not None and anim.event_source is not None:
-        anim.event_source.stop()
-
-    ax.clear()
-    unsorted_array_bars = ax.bar(range(len(arr)), arr, color = "black")
-    ax.title.set_text("Unsorted Array")
-    anim = FuncAnimation(fig, update_fig, frames=[arr], fargs=(unsorted_array_bars,), interval=1, repeat=False, blit=True)
+    bars = ax.bar(range(len(arr)), arr, color = "red")
+    if sort_type == "unsorted":
+        #set the color to black
+        bars = ax.bar(range(len(arr)), arr, color = "black")
+        ax.title.set_text("Unsorted Array")
+        anim = FuncAnimation(fig, update_fig, frames=[arr], fargs=(bars,), interval=1, repeat=False, blit=True)
+    elif sort_type == "bubble":
+        ax.title.set_text("Bubble Sort")
+        anim = FuncAnimation(fig, update_fig, frames=bubbleSort(arr.copy()), fargs=(bars,), interval=20, repeat=False, blit=True, save_count=amount_nums)
+    elif sort_type == "insertion":
+        ax.title.set_text("Insertion Sort")
+        anim = FuncAnimation(fig, update_fig, frames=insertionSort(arr.copy()), fargs=(bars,), interval=20, repeat=False, blit=True, save_count=amount_nums)
+    elif sort_type == "merge":
+        ax.title.set_text("Merge Sort")
+        anim = FuncAnimation(fig, update_fig, frames=mergeSort(arr.copy()), fargs=(bars,), interval=20, repeat=False, blit=True, save_count=amount_nums)
+    
     plt.show()
 
 def randomize(event):
     global arr
     arr = np.random.randint(1, 30, amount_nums)
     ax.clear()
-    unsorted_array_show(None)
+    sort_show("unsorted")
+# def unsorted_array_show(event):
+#     global anim
+#     if anim is not None and anim.event_source is not None:
+#         anim.event_source.stop()
+
+#     ax.clear()
+#     unsorted_array_bars = ax.bar(range(len(arr)), arr, color = "black")
+#     ax.title.set_text("Unsorted Array")
+#     anim = FuncAnimation(fig, update_fig, frames=[arr], fargs=(unsorted_array_bars,), interval=1, repeat=False, blit=True)
+#     plt.show()
 
 button_unsorted = plt.axes([0.02, 0.01, 0.15, 0.05])
 button_unsorted_array = Button(button_unsorted, 'Unsorted Array')
-button_unsorted_array.on_clicked(unsorted_array_show)
+button_unsorted_array.on_clicked(lambda event: sort_show("unsorted"))
 
 button_bubble = plt.axes([0.2, 0.01, 0.15, 0.05])
 button_bubble_sort = Button(button_bubble, 'Bubble Sort')
-button_bubble_sort.on_clicked(bubble_sort_show)
+button_bubble_sort.on_clicked(lambda event: sort_show("bubble"))
 
 button_insertion = plt.axes([0.4, 0.01, 0.17, 0.05])
 button_insertion_sort = Button(button_insertion, 'Insertion Sort')
-button_insertion_sort.on_clicked(insertion_sort_show)
+button_insertion_sort.on_clicked(lambda event: sort_show("insertion"))
 
 button_merge = plt.axes([0.6, 0.01, 0.15, 0.05])
 button_merge_sort = Button(button_merge, 'Merge Sort')
-button_merge_sort.on_clicked(merge_sort_show)
+button_merge_sort.on_clicked(lambda event: sort_show("merge"))
 
-button_random = plt.axes([0.8, 0.01, 0.20, 0.05])
-button_randomize = Button(button_random, 'Generate New Array')
+button_random = plt.axes([0.77, 0.01, 0.15, 0.05])
+button_randomize = Button(button_random, 'Randomize Array')
 button_randomize.on_clicked(randomize)
 
+#to start, run the unsorted_array function (defualt menu)
+anim = None
+sort_show("unsorted")
 plt.show()
